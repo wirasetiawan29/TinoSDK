12310817a36d8d8ed7e4a1c8e5150cfdb3cab29c
Wira Setiawan <wirasetiawan29@gmail.com>
2023-08-24 14:46:35 +0800

    rename

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "TinoSDK",
+    name: "TinodeSDK",
     platforms: [
         .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "TinoSDK",
-            targets: ["TinoSDK"]),
+            name: "TinodeSDK",
+            targets: ["TinodeSDK"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,10 +22,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "TinoSDK",
+            name: "TinodeSDK",
             dependencies: []),
         .testTarget(
-            name: "TinoSDKTests",
-            dependencies: ["TinoSDK"]),
+            name: "TinodeSDKTests",
+            dependencies: ["TinodeSDK"]),
     ]
 )
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# TinoSDK
+# TinodeSDK
 
 A description of this package.
